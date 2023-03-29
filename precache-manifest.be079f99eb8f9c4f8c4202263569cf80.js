320c1a70b79be94f8548d804f4f85ac91e20aec1
Jesse Kevon <jessekevon@gmail.com>
2023-03-29 17:31:25 -0500

    Updates

---
+++ precache-manifest.be079f99eb8f9c4f8c4202263569cf80.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8709bada304b9a3d6a31e8d42fc96c8f",
+    "url": "./index.html"
+  },
+  {
+    "revision": "dfcc5fdfb8c62bedd494",
+    "url": "./static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "b9af760e9c2e97eec9e2",
+    "url": "./static/css/main.a50ec937.chunk.css"
+  },
+  {
+    "revision": "dfcc5fdfb8c62bedd494",
+    "url": "./static/js/2.694ad8ef.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "./static/js/2.694ad8ef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b9af760e9c2e97eec9e2",
+    "url": "./static/js/main.09f679d2.chunk.js"
+  },
+  {
+    "revision": "01dc65dc2ef6dac92472",
+    "url": "./static/js/runtime-main.55ee1e8c.js"
+  }
+]);
